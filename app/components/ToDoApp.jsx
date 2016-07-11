689cca40205917d1d9551b134b395cac3d8522ca
Ralph Kuerbis <ralph@kuerbis.ca>
2016-07-10 22:22:15 -0700

    Initialize file component addons

---
+++ app/components/ToDoApp.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+
+var ToDoApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        ToDoApp.jsx
+      </div>
+    );
+  }
+});
+
+
+module.exports = ToDoApp;
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,14 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components'
+    ],
+
     alias: {
+
+
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']

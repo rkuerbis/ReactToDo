47eec1413c09213a988197d984a81dbafc945513
Ralph Kuerbis <ralph@kuerbis.ca>
2016-10-01 23:28:59 -0700

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var df = require('deep-freeze-strict');
+var df = require('deep-freeze-strict');  // Use to check if parameters pushed to a function stay unchanged within the function
 
 var reducers = require('reducers');
 
@@ -10,7 +10,7 @@ describe('Reducers', () => {
         type: 'SET_SEARCH_TEXT',
         searchText: 'dog'
       };
-      var res = reducers.searchTextReducer('', action);
+      var res = reducers.searchTextReducer(df(''), df(action));
       expect(res).toEqual(action.searchText);
     });
   });
@@ -21,8 +21,43 @@ describe('Reducers', () => {
         type: 'TOGGLE_SHOW_COMPLETED'
 
       };
-      var res = reducers.showCompletedReducer(false, action);
+      var res = reducers.showCompletedReducer(df(false), df(action));
       expect(res).toEqual(true);
     });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '123',
+        text: 'Something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+      // todos variable no longer exists, replaced by res return variable, could be function of df()
+      expect(todos.id).toEqual(undefined);
+    });
+
   });
 });
